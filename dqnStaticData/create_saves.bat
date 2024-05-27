ffe9a3f1e6fd659edcedf79c0807f2b8cfb950c1
Łukasz Ciesielski <l.ciesielski0@gmail.com>
2024-05-27 20:46:29 +0200

    Add create save file structure script #17

---
+++ dqnStaticData/create_saves.bat
@@ -0,0 +1,14 @@
+@echo off
+
+REM Create the first 4 folders
+for /L %%i in (5,1,9) do (
+    mkdir "Save\3300%%i"
+)
+
+REM Create the remaining folders
+for /L %%i in (10,1,50) do (
+    mkdir "Save\330%%i"
+)
+
+echo Folder structure created successfully.
+pause
