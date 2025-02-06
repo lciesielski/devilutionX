fa217236c9cc6aae1f3adfd81b13b0c696b8340d
Łukasz Ciesielski <l.ciesielski0@gmail.com>
2025-02-06 18:50:50 +0100

    Add Debug Item Spawn

--- Source/control.cpp
+++ Source/control.cpp
@@ -582,12 +582,50 @@ std::string TextCmdLevelSeed(const std::string_view parameter)
 	    "Storybook: ", DungeonSeeds[16]);
 }
 
+std::string TextCmdSpawnItem(const std::string_view parameter)
+{
+	std::string ret = "Spawned";
+
+	if (parameter.empty()) {
+		ret = "Provide item name.";
+		return ret;
+	}
+
+	const std::string param = AsciiStrToLower(parameter);
+
+	for (int i = 1; i < 10; i++) {
+		DebugSpawnItem(param);
+	}
+
+	return ret;
+}
+
+std::string TextCmdSpawnUnique(const std::string_view parameter)
+{
+	std::string ret = "Spawned";
+
+	if (parameter.empty()) {
+		ret = "Provide item name.";
+		return ret;
+	}
+
+	const std::string param = AsciiStrToLower(parameter);
+
+	for (int i = 1; i < 10; i++) {
+		DebugSpawnUniqueItem(param);
+	}
+
+	return ret;
+}
+
 std::vector<TextCmdItem> TextCmdList = {
 	{ "/help", N_("Prints help overview or help for a specific command."), N_("[command]"), &TextCmdHelp },
 	{ "/arena", N_("Enter a PvP Arena."), N_("<arena-number>"), &TextCmdArena },
 	{ "/arenapot", N_("Gives Arena Potions."), N_("<number>"), &TextCmdArenaPot },
 	{ "/inspect", N_("Inspects stats and equipment of another player."), N_("<player name>"), &TextCmdInspect },
 	{ "/seedinfo", N_("Show seed infos for current level."), "", &TextCmdLevelSeed },
+	{ "/si", N_("Spawns item."), N_("<item name>"), &TextCmdSpawnItem },
+	{ "/su", N_("Spawns unique item."), N_("<item name>"), &TextCmdSpawnUnique },
 };
 
 bool CheckChatCommand(const std::string_view text)
